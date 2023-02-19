a9b546772151deab52d4efc31953a03dedd1ccba
Deevdv <110405472+Deevdv@users.noreply.github.com>
2023-02-19 20:16:16 +0200

    rev 2

--- pokedex.js
+++ pokedex.js
@@ -7,76 +7,78 @@ function logic() {
 
 
     // Will only give objects
-    console.log( name + " - " + type + " - " + region + " region") // remove once you have clear understanding
+    console.log(name + " - " + type + " - " + region + " region") // remove once you have clear understanding
 
 
     // Get String values of variables
     name = name.value;
     type = type.value;
     region = region.value;
-    
+
 
     // Can log them since they are now pure Strings
-    console.log( name + " - " + type + " - " + region + " region") // remove once you have clear understanding
+    console.log(name + " - " + type + " - " + region + " regions") // remove once you have clear understanding
 
-/*-----------------------------------------------
- * Add your solution code here
- *---------------------------------------------*/
+    /*-----------------------------------------------
+     * Add your solution code here
+     *---------------------------------------------*/
 
     /* --- Part 1 : Create pokemon data Object --- */
 
-				// Use If to ensure input validation (ensure input fields are !empty)
-        if(name === "" || type === "" || region === "") {
-            document.getElementById("empty").innerHTML = "Please enter all fields";
-        }else{
-            document.getElementById("empty").innerHTML = "";
-        };
+    // Use If to ensure input validation (ensure input fields are !empty)
 
-        const pokemon = {'name' : name, 'type':type, 'region' : region};
+    const pokemon = { 'name': name, 'type': type, 'region': region };
 
 
-        // Create a span element (const pokemonData) and put the pokemon objects properties inside of it
-        const pokemonData = document.createElement("span");
-        const pokeName = document.createTextNode(pokemon.name);
-        pokemonData.appendChild(pokeName);
-        const pokeType = document.createTextNode(pokemon.type);
-        pokemonData.appendChild(pokeType);
-        const pokeRegion = document.createTextNode(pokemon.region);
-        pokemonData.appendChild(pokeRegion);
+    // Create a span element (const pokemonData) and put the pokemon objects properties inside of it
+
+    const pokemonData = document.createElement("span");
+    const pokeName = document.createTextNode(pokemon.name);
+    pokemonData.appendChild(pokeName)
+    const pokeType = document.createTextNode(pokemon.type);
+    pokemonData.appendChild(pokeType);
+    const pokeRegion = document.createTextNode(pokemon.region + " region");
+    pokemonData.appendChild(pokeRegion);
+    // para.appendChild(pokeName);
+
 
 
     /* --- Part 2 : Creating other DOM Objects --- */
-        
-        // Create HTML li element here (const pokedexEntry)
-        const pokedexEntry = document.createElement("li");
+
+    // // Create HTML li element here (const pokedexEntry)
+    const pokedexEntry = document.createElement("li");
 
 
-        // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
-        pokedexEntry.className = pokemon.type;
-
-        // Creating a preview button (using a Template Literal)
-        // this code is for an html form that will Google search the name of the pokemon
-        const previewButton = `
+    // // Give pokedexEntry (li) a new class equal to the "type" property of the pokemon object
+    // 
+    pokedexEntry.className = pokemon.type;
+    // Creating a preview button (using a Template Literal)
+    // this code is for an html form that will Google search the name of the pokemon
+    const previewButton = `
             <form action="http://google.com/search" target="_blank">
                 <input name="q" hidden value="${name}">
                 <input type="submit">
             </form>
         `
 
-    
+
     /* --- Part 3 : Add things into the DOM --- */
 
-        // Firstly append pokemonData to the innerHTML of pokedexEntry
-        pokedexEntry.innerHTML += pokemonData;
+    // // Firstly append pokemonData to the innerHTML of pokedexEntry
+    pokedexEntry.innerHTML += pokemonData.innerText;
 
-        // Secondly append the previewButton to innerHTML of pokedexEntry
-        pokedexEntry.innerHTML += previewButton;
+    // Secondly append the previewButton to innerHTML of pokedexEntry
+    pokedexEntry.innerHTML += previewButton;
 
-        // Lastly we will get the ul List element that is meant to contain all pokedex entries
-        // The we will append pokedexEntry to the innerHTML of the pokedexEntries ul element
-        pokedexEntry.innerHTML =+ pokedexEntries;
+    // // Lastly we will get the ul List element that is meant to contain all pokedex entries
+    const element = document.getElementById("pokedex-entries");
+    // // The we will append pokedexEntry to the innerHTML of the pokedexEntries ul element
+    element.appendChild(pokedexEntry)
+    // pokedexEntry.innerHTML =+ pokedexEntry;
 
-/*-----------------------------------------------
- * Solution must be above this comment
- *---------------------------------------------*/
+    // element.appendChild(pokemonData);
+
+    /*-----------------------------------------------
+     * Solution must be above this comment
+     *---------------------------------------------*/
 }
